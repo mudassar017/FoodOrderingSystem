b3b26d6384c4bd2be1f84b2e8fde02cffcdfda99
Arsalan Ahmad <74791441+arslanmgl9@users.noreply.github.com>
2021-06-01 18:44:26 +0500

    Commented In JWT's Classs and Services in Startup

--- FoodOrderingSystem/Management Classes/JWT's.cs
+++ FoodOrderingSystem/Management Classes/JWT's.cs
@@ -7,28 +7,28 @@ namespace FoodOrderingSystem.Management_Classes
 {
     public class JWT_s
     {
-        public static string GenerateJSONWebToken(Student userInfo, IConfiguration config)
-        {
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+        //public static string GenerateJSONWebToken(Student userInfo, IConfiguration config)
+        //{
+        //    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+        //    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
-            //var claims = new[] {
-            //new Claim(JwtRegisteredClaimNames.Sub, userInfo.Username) };
+        //    //var claims = new[] {
+        //    //new Claim(JwtRegisteredClaimNames.Sub, userInfo.Username) };
 
-            var claims = new[] {
-             new Claim("UserId", userInfo.StudentId.ToString()),
-             new Claim("UserName", userInfo.UserName.ToString())
+        //    var claims = new[] {
+        //     new Claim("UserId", userInfo.StudentId.ToString()),
+        //     new Claim("UserName", userInfo.UserName.ToString())
 
-            };
+        //    };
 
 
-            var token = new JwtSecurityToken(config["Jwt:Issuer"],
-              config["Jwt:Issuer"],
-              claims,
-              expires: DateTime.Now.AddMinutes(120),
-              signingCredentials: credentials);
+        //    var token = new JwtSecurityToken(config["Jwt:Issuer"],
+        //      config["Jwt:Issuer"],
+        //      claims,
+        //      expires: DateTime.Now.AddMinutes(120),
+        //      signingCredentials: credentials);
 
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
+        //    return new JwtSecurityTokenHandler().WriteToken(token);
+        //}
     }
 }
--- FoodOrderingSystem/Startup.cs
+++ FoodOrderingSystem/Startup.cs
@@ -24,20 +24,20 @@ namespace FoodOrderingSystem
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-     .AddJwtBearer(options =>
-     {
-         options.TokenValidationParameters = new TokenValidationParameters
-         {
-             ValidateIssuer = true,
-             ValidateAudience = true,
-             ValidateLifetime = false,
-             ValidateIssuerSigningKey = true,
-             ValidIssuer = Configuration["Jwt:Issuer"],
-             ValidAudience = Configuration["Jwt:Issuer"],
-             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
-         };
-     });
+     //       services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+     //.AddJwtBearer(options =>
+     //{
+     //    options.TokenValidationParameters = new TokenValidationParameters
+     //    {
+     //        ValidateIssuer = true,
+     //        ValidateAudience = true,
+     //        ValidateLifetime = false,
+     //        ValidateIssuerSigningKey = true,
+     //        ValidIssuer = Configuration["Jwt:Issuer"],
+     //        ValidAudience = Configuration["Jwt:Issuer"],
+     //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+     //    };
+     //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
