a8f6275f9ac820b7bda800a0f12b300a5f3190bd
Mian Azhar <mianazhar986@gmail.com>
2021-06-02 16:20:36 +0500

    Encrypti

---
+++ FoodOrderingSystem/EncryptionDecryptionClass/EncryptDecrypt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrderingSystem.EncryptionDecryptionClass
+{
+    public class EncryptDecrypt
+    {
+
+        public static string key = "sblw-3hn8-sqoy19";
+        public static string Base64Encode(string plainText)
+        {
+            //var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            //return System.Convert.ToBase64String(plainTextBytes);
+            byte[] inputArray = UTF8Encoding.UTF8.GetBytes(plainText);
+            TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider();
+            tripleDES.Key = UTF8Encoding.UTF8.GetBytes(key);
+            tripleDES.Mode = CipherMode.ECB;
+            tripleDES.Padding = PaddingMode.PKCS7;
+            ICryptoTransform cTransform = tripleDES.CreateEncryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
+            tripleDES.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+        public static string Base64Decode(string base64EncodedData)
+        {
+            //var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            //return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+            byte[] inputArray = Convert.FromBase64String(base64EncodedData);
+            TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider();
+            tripleDES.Key = UTF8Encoding.UTF8.GetBytes(key);
+            tripleDES.Mode = CipherMode.ECB;
+            tripleDES.Padding = PaddingMode.PKCS7;
+            ICryptoTransform cTransform = tripleDES.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
+            tripleDES.Clear();
+            return UTF8Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
