8fce56038d79089e0b8ebb0077685eb6bc6a7790
Mudassar Irshad <mudassar.irshad2@outlook.com>
2021-06-05 04:16:22 -0700

    Entites Table

---
+++ FoodOrderingSystem/DB/Food.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodOrderingSystem.DB
+{
+    public class Food
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int FoodId { get; set; }
+        public int MenuId { get; set; }
+        public string FoodName { get; set; }
+        public string FoodDescription { get; set; }
+        public int FoodPrice { get; set; }
+        public DateTime RatingonFood { get; set; }
+        public DateTime InsertionDateTime { get; set; }
+    }
+}
