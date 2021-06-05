a9af983e6d6a698a41c5d96ea3981ff1331b663a
Arsalan Ahmad <74791441+arslanmgl9@users.noreply.github.com>
2021-06-05 19:52:50 +0500

    AdminLayout Implementation

---
+++ FoodOrderingSystem/Controllers/AdminPannelController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodOrderingSystem.Context
+{
+    public class AdminPannelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
